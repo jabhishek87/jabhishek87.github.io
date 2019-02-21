da595caf58a7549dbd315f2b95a5969dc0aee1cc
AbhishekJ <ia.malhotra@gmail.com>
2019-02-22 00:29:52 +0530

    new design

---
+++ js/render.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+    $.get('data/data.yaml').done(function (data) {
+        console.log('File load complete');
+        JsonData = jsyaml.load(data)
+        //console.log(JsonData);
+        // Replace with dynameic value
+        $('#first_name').text(JsonData.first_name);
+        $('#last_name').text(JsonData.last_name);
+        $('#physical_address').text(JsonData.physical_address);
+        $('#email_address').text(JsonData.email_address);
+        $("#email_address").attr("href", "mailto:" + JsonData.email_address);
+        $('#dy_fullname').text(JsonData.first_name + " " + JsonData.last_name);
+
+        $("#summary").text(JsonData.summary);
+
+        //experience block
+        var experience_template = $("#experience-template").html();
+        $.each(JsonData.experience, function( index, value ) {
+            $.tmpl(experience_template, {"data": value}).appendTo("#dy_experience");
+        });
+
+        //education block
+        var experience_template = $("#education-template").html();
+        $.each(JsonData.education, function( index, value ) {
+            $.tmpl(experience_template, {"data": value}).appendTo("#dy_education");
+        });
+
+        //skills block $.parseHTML( str ),
+        var experience_template = $("#skills-template").html();
+        $.each(JsonData.skills, function( index, value ) {
+            $.tmpl(experience_template, {"skill": value}).appendTo("#dy_skills");
+        });
+
+        //awards block
+        var experience_template = $("#awards-template").html();
+        $.each(JsonData.achievements, function( index, value ) {
+            $.tmpl(experience_template, {"award": value}).appendTo("#dy_awards");
+        });
+
+        //social links block
+        var experience_template = $("#social-template").html();
+        $.each(JsonData.social_accounts, function( index, value ) {
+            $.tmpl(experience_template, {"social_account": value}).appendTo("#dy_social");
+        });
+
+        //Portfolio Datatable Block
+        $('#tbl_portfolio').DataTable({
+            "pageLength": 50,
+            "ajax": "data/repos_details.json",
+            "columns": [
+            {"data": "repo_full_name"},
+            {"data": "repo_description"},
+            {
+                "data": "repo_url",
+                "render": function(data, type, row, meta){
+                    if(type === 'display'){
+                        data = '<a href="' + data + '">' + data + '</a>';
+                    }
+                    return data;
+                }
+            },
+            {"data": "repo_readme"}
+            ],
+            "columnDefs": [{
+                "targets": [ 3 ],
+                "visible": false,
+                "searchable": true
+            }]
+        });
+
+    });
+    //
+});
+
+// (function($) {
+//     "use strict"; // Start of use strict
+
+//   })(jQuery); // End of use strict
